7ea0261ccd9b6aed34876f4eba531c4579e51bdb
akbarfajar21 <akbarfajar2112@gmail.com>
2025-07-17 17:53:20 +0700

    fix: corrected Supabase import syntax

--- src/utils/SupaClient.js
+++ src/utils/SupaClient.js
@@ -1,8 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
 
-import { createClient } from '`@supabase/supabase-js`';
-
-const supabaseUrl = 'https://slfytjpkrfvbyuhxnjrd.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNsZnl0anBrcmZ2Ynl1aHhuanJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIzMzEwNzIsImV4cCI6MjA2NzkwNzA3Mn0.py2FBGnDNn6cv9GF7t_kAI0YuVmgen7DLOjtURme8iI';
+const supabaseUrl = "https://slfytjpkrfvbyuhxnjrd.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNsZnl0anBrcmZ2Ynl1aHhuanJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIzMzEwNzIsImV4cCI6MjA2NzkwNzA3Mn0.py2FBGnDNn6cv9GF7t_kAI0YuVmgen7DLOjtURme8i";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
